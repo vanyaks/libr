ff11f6ff38d6c69a4d6de3094b76d423aaaeb0fa
vanyaks <44755576+vanyaks@users.noreply.github.com>
2018-11-05 21:31:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const server = http.createServer((req, res) => {
     var i=0, BookId;
     switch(req.url.substring(0,10)) {
         case '/api/books':
-            res.writeHead(200, { 'Content-Type': 'text/jacascript' });
-            res.end(books);
+            res.writeHead(200, { 'Content-Type': 'text/javascript; charset=utf-8' });
+            for (var k=0; k<JSON.parse(books).length; k++) {
+                res.write(JSON.parse(books)[k]["id"] +' | '+ JSON.parse(books)[k]["name"] +' | '+ JSON.parse(books)[k]["author"] +' | '+ JSON.parse(books)[k]["description"] +'\n');
+            };
+            res.end();
             break;
         case '/api/book/' :
             BookId = parseInt(req.url.slice(-3));
             while (JSON.parse(books)[i]["id"]!=BookId) {
                 i++;
             };
-            res.writeHead(200, { 'Content-Type': 'text/javascript' });
-            res.end(JSON.parse(books)[i]["id"]);
+            res.writeHead(200, { 'Content-Type': 'text/javascript; charset=utf-8' });
+            res.end(JSON.parse(books)[i]["id"] +' | '+ JSON.parse(books)[i]["name"] +' | '+ JSON.parse(books)[i]["author"] +' | '+ JSON.parse(books)[i]["description"]);
             break;
         default:
             res.writeHead(200, { 'Content-Type': 'text/plain' });
